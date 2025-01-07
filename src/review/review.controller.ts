90d7e73a525880524b7f9b3cf98c55a097a8b660
lakshaymor4 <117587156+lakshaymor4@users.noreply.github.com>
2025-01-07 23:41:46 +0530

    review tested and implemented

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,4 +1,85 @@
-import { Controller } from '@nestjs/common';
+import {
+    Controller,
+    Body,
+    Get,
+    Post,
+    HttpCode,
+    HttpStatus,
+    Request,
+    UseGuards,
+    Put,
+    Patch,
+    Param,
+    Delete,
+} from '@nestjs/common';
+import { ReviewService } from './review.service';
+import { AuthGuard } from 'src/auth/auth.guard';
+
+
 
 @Controller('review')
-export class ReviewController {}
+export class ReviewController {
+    constructor(private reviewservice: ReviewService) { }
+
+    @UseGuards(AuthGuard)
+    @HttpCode(HttpStatus.CREATED)
+    @Post('create')
+    async create(
+        @Body() data: { restrauntId: number, review: string },
+        @Request() req) {
+        const transformedData = {
+            restraunt: {
+                connect: { id: data.restrauntId },
+            },
+            user: {
+                connect: { id: req.user.sub },
+            },
+            review: data.review,
+        };
+        return this.reviewservice.createReview(transformedData);
+    }
+
+    @UseGuards(AuthGuard)
+    @HttpCode(HttpStatus.OK)
+    @Patch('update')
+
+    async update(
+        @Body() data: { id: number, review: string },
+        @Request() req
+    ) {
+        return this.reviewservice.updateReview({
+            where: { id: data.id },
+            data: {
+                userId: req.user.sub,
+                review: data.review,
+            },
+        });
+
+    }
+    @UseGuards(AuthGuard)
+    @HttpCode(HttpStatus.OK)
+    @Delete('delete')
+    async delete(
+        @Body() data: { id: number },
+        @Request() req
+    ) {
+        return this.reviewservice.deleteReview({
+            where: { id: data.id },
+            data: {
+                userId: req.user.sub,
+            },
+        });
+    }
+
+    @UseGuards(AuthGuard)
+    @HttpCode(HttpStatus.OK)
+    @Get('getall')
+    async getAll(
+        @Request() req
+    ) {
+        return this.reviewservice.getReviewByUserId({ id: req.user.sub });
+    }
+
+
+
+}
--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -1,70 +1,85 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 import { User, Restraunt, Prisma, Review } from '@prisma/client';
+import { ExceptionsHandler } from '@nestjs/core/exceptions/exceptions-handler';
 
 @Injectable()
 export class ReviewService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) { }
 
-  async getReviews(params: { id: number }): Promise<Review[]> {
-    const { id } = params;
-    return this.prisma.review.findMany({
-      where:{
-      restrauntId: id,
-      }
-    });
-  }
 
   async getReviewByUserId(params: { id: number }): Promise<Review[]> {
     const { id } = params;
     return this.prisma.review.findMany({
-      where:{
-      userId: id,
+      where: {
+        userId: id,
       }
     });
   }
 
- async createReview(data: Prisma.ReviewCreateInput): Promise<Review> {
+  async createReview(data: Prisma.ReviewCreateInput): Promise<Review> {
 
-  const restrauntId = data.restraunt.connect?.id;
+    const restrauntId = data.restraunt.connect?.id;
 
-  if (!restrauntId) {
-    throw new Error('Invalid restaurant ID');
-  }
+    if (!restrauntId) {
+      throw new Error('Invalid restaurant ID');
+    }
 
- 
-  const restraunt = await this.prisma.restraunt.findUnique({
-    where: { id: restrauntId },
-  });
 
-  if (!restraunt) {
-    throw new Error('Restaurant not found');
-  }
-
-  
-  if (restraunt.ownerId !== data.user.connect?.id) {
-    return this.prisma.review.create({
-      data,
+    const restraunt = await this.prisma.restraunt.findUnique({
+      where: { id: restrauntId },
     });
-  } else {
-    throw new Error('Action Not Allowed');
+
+    if (!restraunt) {
+      throw new Error('Restaurant not found');
+    }
+
+
+    if (restraunt.ownerId !== data.user.connect?.id) {
+      return this.prisma.review.create({
+        data,
+      });
+    } else {
+      throw new Error('Action Not Allowed');
+    }
   }
-}
 
   async updateReview(params: {
     where: Prisma.ReviewWhereUniqueInput;
-    data: Prisma.ReviewUpdateInput;
+    data: { userId: number, review: string };
   }): Promise<Review> {
     const { data, where } = params;
+    const findReview = await this.prisma.review.findUnique({
+      where,
+    });
+    if (findReview.userId !== data.userId) {
+      throw new UnauthorizedException;
+    }
     return this.prisma.review.update({
-      data,
+      data: {
+        review: data.review,
+      },
       where,
     });
   }
 
-  async deleteReview(where: Prisma.ReviewWhereUniqueInput): Promise<Review> {
-    return this.prisma.review.delete({
+  async deleteReview(params: { where: Prisma.ReviewWhereUniqueInput; data: { userId: number } }): Promise<{ success: boolean }> {
+    const { where, data } = params;
+    const findReview = await this.prisma.review.findUnique({
       where,
     });
+    if (findReview.userId !== data.userId) {
+      throw new UnauthorizedException;
+    }
+    const delReview = await this.prisma.review.delete({
+      where,
+    });
+
+    if (delReview) {
+      return { success: true };
+    }
+    else {
+      throw new ExceptionsHandler
+    }
   }
 }
