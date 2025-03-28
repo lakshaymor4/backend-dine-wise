fb516cb0a58f277279201594856aa23726a4de3d
lakshaymor4 <117587156+lakshaymor4@users.noreply.github.com>
2025-03-28 21:18:37 +0530

    updated cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://dinewisee.vercel.app',
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
